25f5fe8f3088e16035de48af1509787cd98b4d21
engaweis <maxamedaweys90@gmail.com>
2025-06-12 10:28:42 +0300

    updated the dev link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="footer-container">
         <div className="footer-info">
           <p>&copy; 2025 LandingPage. All rights reserved. &nbsp;</p>
-          <h4>Dev:&nbsp; <a href="https://engaweis.netlify.app/" target="_blank" rel="noopener noreferrer">Eng-Aweis</a></h4>
+          <h4>Dev:&nbsp; <a href="https://engaweis.space/" target="_blank" rel="noopener noreferrer">Eng-Aweis</a></h4>
         </div>
         <ul className="footer-links">
           <li><a href="#privacy">Privacy Policy</a></li>
